a91155aa76b3ae0ced956c4950c5824089486611
Egor <el.amegoal@gmail.com>
2025-08-01 10:24:38 +0300

    added common-jpa module and entities

---
+++ common-jpa/src/main/java/by/psrer/entity/Route.java
@@ -0,0 +1,31 @@
+package by.psrer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "route_id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "route")
+public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long routeId;
+    private String routeDescription;
+    private String isActivity;
+}
---
+++ common-jpa/src/main/java/by/psrer/entity/enums/UserState.java
@@ -0,0 +1,7 @@
+package by.psrer.entity.enums;
+
+public enum UserState {
+    BASIC,
+    ROUTE_SELECTION,
+    QUESTION_SELECTION
+}
